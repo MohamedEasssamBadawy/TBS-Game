8b95df5f00e22cef1724cab6560b2a1566abf7eb
Mohamed Badawy <MEassambadawy@gmail.com>
2025-01-01 08:40:27 +0200

    # Project Files

---
+++ TurnBasedStrategyGame/Assets/Scripts/Testing.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            GridSystemVisual.Instance.HideAllGridPosition();
+            GridSystemVisual.Instance.ShowGridPositionList(
+            unit.GetMoveAction().GetValidActionGridPositionList());
+        }
+    }
+}
---
+++ TurnBasedStrategyGame/Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake() {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start() {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+
+        UpdateVisual();
+    }
+
+    private void OnDisable() {
+        UnitActionSystem.Instance.OnSelectedUnitChanged -= UnitActionSystem_OnSelectedUnitChanged;
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, System.EventArgs e) {
+
+        UpdateVisual();
+    }
+
+    private void UpdateVisual() {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit) {
+            meshRenderer.enabled = true;
+        }
+        else {
+            meshRenderer.enabled = false;
+        }
+    }
+}
