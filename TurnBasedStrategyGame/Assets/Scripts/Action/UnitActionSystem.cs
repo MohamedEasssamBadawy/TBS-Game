57ebd3ace33386b9ac51bc4a8983beec4072f600
Mohamed Badawy <MEassambadawy@gmail.com>
2025-02-22 10:17:21 +0200

    Added input system again after modified

--- TurnBasedStrategyGame/Assets/Scripts/Action/UnitActionSystem.cs
+++ TurnBasedStrategyGame/Assets/Scripts/Action/UnitActionSystem.cs
@@ -50,7 +50,7 @@ public class UnitActionSystem : MonoBehaviour
     }
 
     private void HandleSelectedAction() {
-        if (InputManager.Instance.IsMouseButtonDown()) {
+        if (InputManager.Instance.IsMouseButtonDownThisFrame()) {
 
             GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
 
@@ -82,7 +82,7 @@ public class UnitActionSystem : MonoBehaviour
     }
 
     private bool TryHandleUnitSelection() {
-        if (InputManager.Instance.IsMouseButtonDown()) {
+        if (InputManager.Instance.IsMouseButtonDownThisFrame()) {
             Ray ray = Camera.main.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
             if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask)) {
                 if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit)) {
--- TurnBasedStrategyGame/Assets/Scripts/InputManager.cs
+++ TurnBasedStrategyGame/Assets/Scripts/InputManager.cs
@@ -1,8 +1,12 @@
+#define USE_NEW_INPUT_SYSTEM
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour {
 
     public static InputManager Instance { get; private set; }
+
+    private PlayerInputActions playerInputActions;
 
     private void Awake() {
         if (Instance != null) {
@@ -12,59 +16,75 @@ public class InputManager : MonoBehaviour {
         }
         Instance = this;
 
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
     }
 
     public Vector2 GetMouseScreenPosition() {
+#if USE_NEW_INPUT_SYSTEM
+        return Mouse.current.position.ReadValue();
+#else
         return Input.mousePosition;
+#endif
     }
 
-    public bool IsMouseButtonDown() {
+    public bool IsMouseButtonDownThisFrame() {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.Click.WasPressedThisFrame();
+#else
         return Input.GetMouseButtonDown(0);
+#endif
     }
 
     public Vector2 GetCameraMoveVector() {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraMovement.ReadValue<Vector2>();
+#else
         Vector2 inputMoveDir = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W)) {
-            inputMoveDir.y = +1f;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputMoveDir.y = -1f;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputMoveDir.x = +1f;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputMoveDir.x = -1f;
+            
         }
 
         return inputMoveDir;
+#endif
     }
 
     public float GetCameraRotateAmount() {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraRotate.ReadValue<float>();
+#else
         float rotateAmount = 0f;
 
         if (Input.GetKey(KeyCode.Q)) {
-            rotateAmount = +1f;
+            roateAmount = +1f;
         }
+
         if (Input.GetKey(KeyCode.E)) {
-            rotateAmount = -1f;
+            roateAmount = -1f;
         }
 
         return rotateAmount;
+#endif
     }
 
     public float GetCameraZoomAmount() {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraZoom.ReadValue<float>();
+#else
+
         float zoomAmount = 0f;
 
         if (Input.mouseScrollDelta.y > 0) {
             zoomAmount = -1f;
         }
+
         if (Input.mouseScrollDelta.y < 0) {
-            zoomAmount += +1f;
+            zoomAmount = +1f;
         }
 
         return zoomAmount;
+#endif
     }
 
 }
